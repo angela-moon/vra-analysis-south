0a528aeabc53b8151ee9c6e375269739d3750c1c
angela-moon <angmoonn@gmail.com>
2026-06-26 17:07:41 -0400

    FL Recom and SMC ensembles

---
+++ FL/Redist Analysis/FL_Redist.R
@@ -0,0 +1,48 @@
+library(alarmdata)
+library(redist)
+library(tidyverse)
+library(sf)
+library(data.table)
+
+state= "FL"
+nsims_per_run= 2500
+runs= 2
+ncores=1
+seed= 2
+pop_tol = 0.01
+compactness= 1
+pop_temper= 0.05
+use_county_constraint= TRUE
+out_dir= "outputs_sc_vra_off"
+
+map <- alarm_50state_map("FL")
+
+set.seed(seed)
+
+county_arg <- if (isTRUE(use_county_constraint)) map$county else NULL
+constraints_vra_off <- redist::redist_constr(map)  # could vary based on the alarm setup? Some states only had VRA though
+
+plans_vra_off <- redist::redist_smc(
+  map = map,
+  nsims = nsims_per_run,
+  runs = runs,
+  ncores = ncores,
+  compactness = compactness,
+  pop_temper = pop_temper,
+  counties = county_arg,
+  constraints = constraints_vra_off,
+  ref_name = "cd_2020",
+  verbose = TRUE
+) |>
+  redist::match_numbers("cd_2020")
+
+plans_sampled <- redist::subset_sampled(plans_vra_off)
+
+summary(plans_sampled)
+
+plan_mat <- redist::get_plans_matrix(plans_sampled)
+
+rownames(plan_mat) <- map$GEOID
+
+fwrite(as.data.frame(plan_mat), "fl_alarm_plans.csv", row.names = TRUE)
+
